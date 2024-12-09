3646874edf74bd08eb5666b7a7c8b84aa4aeb25c
Mohith Kumar <155897668+b0tProgrammer@users.noreply.github.com>
2024-12-09 16:52:34 +0530

    Add files via upload

---
+++ Grouping Increases.java
@@ -0,0 +1,290 @@
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class Main {
+
+    private static final long lif = (long) 1e9 + 7; //long infinity
+    private static final int iif = 998244353; // int infinity!
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+//            Comparator<Pair> com = Comparator.comparingInt(i -> i.v1);
+
+            int testcases = 1;
+
+            testcases = in.nextInt();
+
+            while(testcases-- > 0) {
+
+                int siz = in.nextInt();
+
+                int last1 = iif; // we need only that last elements of the subSequences so we use it like this!
+
+                int last2 = iif;
+
+                int penalty = 0; // count the penalty!
+
+                for(int i = 0; i < siz; i++) {
+
+                    int val = in.nextInt();
+
+                    if(last2 > last1) {
+                        int t = last2;
+                        last2 = last1;
+                        last1 = t;
+                    }
+
+                    if(val <= last2) { // if the value is less then or equal to the min of last two elements!
+                        last2 = val;
+                    } else if(val <= last1) { // else the value is between the smaller and greater elements of last two elements
+                        last1 = val; // the max element is being updated!
+                    } else { // if the val is greater than both of the values of the array! updating the min val!
+                        last2 = val;
+                        penalty++;
+                    }
+
+                    /*
+
+                    In this case we followed a greedy path so that let's take an example!
+
+                    the last elements of those arrays be
+                    If the element 4 wants to be added then where we will add that!
+
+                    7 -> last element of 1st array!
+
+                    3 -> last element of 2nd array!
+
+                    if we add 4 to the first array no penalty but the range is decreased!
+                    else add penalty and extend the range!
+
+                    In these cases we have to think of the current situation.
+                    Because if we didn't take the penalty now then in future there might be a chance of penalty/ or may not!
+
+                    so, there is an equal chances for both the things so, it's better to skip penalty at current position!
+
+                     */
+
+                }
+                out.println(penalty);
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    //<--------------------------------------Fast reader----------------------------------------->
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+//<---------------------------------------Fast writer--------------------------------------------->
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println() throws IOException {
+            bw.append("\n");
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+// <-------------------------------------------------Some helper methods---------------------------------------->
+
+    private static char check(int i, int siz,String s) {
+        for(char c = 'a'; c < 'z'; c++) {
+            if(c != s.charAt(i-1)) {
+                if(i == siz-1) return c;
+                else if(c != s.charAt(i+1)) return c;
+            }
+        }
+        return 'z';
+    }
+
+    private static long kadane(int l, int r, int[] nums) {
+        long sum = 0;
+        long mSum = 0;
+        for(int i = l; i < r; i++) {
+            if(sum < 0) sum = nums[i];
+            else sum += nums[i];
+            mSum = max(sum,mSum);
+        }
+        return mSum;
+    }
+
+    public static List<Integer> reversed(List<Integer> lis) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = lis.size()-1; i >= 0; i--) {
+            ans.add(lis.get(i));
+        }
+        return ans;
+    }
+
+    private static int update(int ans, int i) {
+        return (int)((ans*1L*i)%iif);
+    }
+
+    private static void getFactors(int num, Map<Integer, Integer> map) {
+
+        for(int i = 2; i <= (int) Math.sqrt(num); i++) {
+            while(num%i == 0) {
+                map.put(i,map.getOrDefault(i,0)+1);
+                num /= i;
+            }
+        }
+        if(num > 1) map.put(num,map.getOrDefault(num,0)+1);
+    }
+
+    public static void print(boolean cond,FastWriter out) throws IOException {
+        out.println(cond ? "Yes" : "No");
+    }
+
+    public static long fastPow(long b, long e) {
+        long res = 1;
+        while (e > 0) {
+            if (odd(e)) res = (res * b) % lif;
+            b = (b * b) % lif;
+            e = e >> 1;
+        }
+        return res;
+    }
+
+    public static int charToInt(char c) {
+        return (c - '0');
+    }
+
+    public static char intToChar(int n) {
+        return (char) (n + 48);
+    }
+
+    public static int[] sieve(int upto) {
+        int[] primes = new int[upto + 1];
+        Arrays.fill(primes, 1);
+        for (int i = 2; i * i <= upto; i++) {
+            if (primes[i] == 1)
+                for (int j = i * i; j <= upto; j += i) {
+                    primes[j] = 0;
+                }
+        }
+        return primes;
+    }
+
+    private static long gcd(long a, long b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static long getSum(int startNum, int commonDiff, long range) {
+        long lastNum = startNum + (range - 1) * commonDiff;
+        return range * (startNum + lastNum) / 2;
+    }
+
+    private static boolean odd(long siz) {
+        return (siz & 1) == 1;
+    }
+
+    private static boolean powOf2(long siz) {
+        return (siz & (siz - 1)) == 0;
+    }
+
+    private static List<Integer> primeFactors(int siz) {
+
+        List<Integer> sizs = new ArrayList<>();
+        int[] p;
+        p = sieve(siz);
+        for (int i = 2; i <= siz; i++) {
+            while (siz % i == 0 && p[i] == 1) {
+                sizs.add(i);
+                siz /= i;
+            }
+        }
+        return sizs;
+    }
+}
+
+class Pair2 {
+    long v1;
+    long v2;
+
+    public Pair2(long v1, long v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+}
+class Pair {
+    int v1,v2;
+    public Pair(int v,int w) {v1 = v; v2 = w;}
+}
+class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int data) {
+        this.data = data;
+    }
+}
