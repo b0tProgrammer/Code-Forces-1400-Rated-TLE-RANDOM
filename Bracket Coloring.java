b844c644da7be05c3bd642ad35b6f55883f9084a
Mohith Kumar <155897668+b0tProgrammer@users.noreply.github.com>
2024-12-13 16:00:52 +0530

    Add files via upload

---
+++ Bracket Coloring.java
@@ -0,0 +1,286 @@
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.*;
+
+
+public class Main {
+
+    private static final int mod = (int) 1e9 + 7; //mod
+    private static final int iif = 998244353; // int infinity!
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+//            Comparator<Pair> com = Comparator.comparingInt(i -> i.v1);
+
+            int testcases = 1;
+
+            testcases = in.nextInt();
+
+            while(testcases-- > 0) {
+
+                /*
+                    Credits : SecondThread
+                 */
+
+                int siz = in.nextInt(); // taking size as input!
+
+                char[] braces = in.next().toCharArray(); // taking the string input
+                int count = 0; // if the overall brackets count is not equal to zero, then it's no use the answer is -1
+                boolean isPos = false; // if the value of count is positive update this to true;
+                boolean isNeg = false; // if the value of count is negative update this to true;
+                for(char c : braces) {
+                    if(c == '(') count++; // counting the open brackets as positive
+                    else count--; // else decreasing the count
+                    if(count > 0) isPos = true;
+                    else if(count < 0) isNeg = true;
+                }
+                if(count != 0) out.println(-1);
+                else {
+                    if(!isPos || !isNeg) { // this states that while counting we came across any one of the positive and negative counts so by reversing, we can make the string balanced!
+                        out.println(1);
+                        for(int i = 0; i < siz; i++) out.print("1 ");
+                    }
+                    else {
+                        out.println(2); // else the answer is 2
+                        int co = 0;
+                        for(char c : braces) {
+                            int old = co;
+                            if(c == '(') co++;
+                            else co--;
+                            if(old != 0) {
+                                out.print((old > 0) ? 1 : 2); // if the count till that point is non-zero, then if the count is even will give that a value 1
+                            }
+                            else {
+                                out.print((co > 0) ? 1 : 2); // if the count at the current value is less then 0 then make a separate group or else the same group!
+                            }
+                            out.print(" ");
+                        }
+                    }
+                    out.println();
+                }
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    //<--------------------------------------Fast reader----------------------------------------->
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+//<---------------------------------------Fast writer--------------------------------------------->
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println() throws IOException {
+            bw.append("\n");
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+// <-------------------------------------------------Some helper methods---------------------------------------->
+
+    private static boolean check(int mid, Pair[] ranges) {
+
+        int siz = ranges.length;
+        int currL = 0;
+        int currR = mid;
+        for (Pair range : ranges) {
+            if (range.v1 > currR || range.v2 < currL) return false;
+            currL = max(currL, range.v1);
+            currL = max(0, currL - mid);
+            currR = min(currR, range.v2);
+            currR += mid;
+        }
+        return true;
+    }
+
+    private static long kadane(int l, int r, int[] nums) {
+        long sum = 0;
+        long mSum = 0;
+        for(int i = l; i < r; i++) {
+            if(sum < 0) sum = nums[i];
+            else sum += nums[i];
+            mSum = max(sum,mSum);
+        }
+        return mSum;
+    }
+
+    public static List<Integer> reversed(List<Integer> lis) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = lis.size()-1; i >= 0; i--) {
+            ans.add(lis.get(i));
+        }
+        return ans;
+    }
+
+    private static int update(int ans, int i) {
+        return (int)((ans*1L*i)%iif);
+    }
+
+    private static void getFactors(int num, Map<Integer, Integer> map) {
+
+        for(int i = 2; i <= (int) Math.sqrt(num); i++) {
+            while(num%i == 0) {
+                map.put(i,map.getOrDefault(i,0)+1);
+                num /= i;
+            }
+        }
+        if(num > 1) map.put(num,map.getOrDefault(num,0)+1);
+    }
+
+    public static void print(boolean cond,FastWriter out) throws IOException {
+        out.println(cond ? "Yes" : "No");
+    }
+
+    public static long fastPow(long b, long e) {
+        long res = 1;
+        while (e > 0) {
+            if (odd(e)) res = (res * b) % mod;
+            b = (b * b) % mod;
+            e = e >> 1;
+        }
+        return res;
+    }
+
+    public static int charToInt(char c) {
+        return (c - '0');
+    }
+
+    public static char intToChar(int n) {
+        return (char) (n + 48);
+    }
+
+    public static int[] sieve(int upto) {
+        int[] primes = new int[upto + 1];
+        Arrays.fill(primes, 1);
+        for (int i = 2; i * i <= upto; i++) {
+            if (primes[i] == 1)
+                for (int j = i * i; j <= upto; j += i) {
+                    primes[j] = 0;
+                }
+        }
+        return primes;
+    }
+
+    private static long gcd(long a, long b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static long getSum(int startNum, int commonDiff, long range) {
+        long lastNum = startNum + (range - 1) * commonDiff;
+        return range * (startNum + lastNum) / 2;
+    }
+
+    private static boolean odd(long siz) {
+        return (siz & 1) == 1;
+    }
+
+    private static boolean powOf2(long siz) {
+        return (siz & (siz - 1)) == 0;
+    }
+
+    private static List<Integer> primeFactors(int siz) {
+
+        List<Integer> sizs = new ArrayList<>();
+        int[] p;
+        p = sieve(siz);
+        for (int i = 2; i <= siz; i++) {
+            while (siz % i == 0 && p[i] == 1) {
+                sizs.add(i);
+                siz /= i;
+            }
+        }
+        return sizs;
+    }
+}
+
+class Pair2 {
+    long v1;
+    long v2;
+
+    public Pair2(long v1, long v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+}
+class Pair {
+    int v1,v2;
+    public Pair(int v,int w) {v1 = v; v2 = w;}
+}
+class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int data) {
+        this.data = data;
+    }
+}
